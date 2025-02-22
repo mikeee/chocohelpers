41f708494b66e759ddde30760b0470bd5317f737
Mike Nguyen <hey@mike.ee>
2025-02-22 15:40:32 +0000

    feat: add chromedp opts with ua

--- regexmatch.go
+++ regexmatch.go
@@ -8,8 +8,41 @@ import (
 	"github.com/chromedp/chromedp"
 )
 
+type Options struct {
+	UserAgent string
+}
+
+type optionFunction func(*Options)
+
+func WithUserAgent(ua string) optionFunction {
+	return func(o *Options) {
+		o.UserAgent = ua
+	}
+}
+
 func RegexMatch(url string, matchString string, matchIndex int) (rawRegex string, err error) {
-	ctx, cancel := chromedp.NewContext(context.Background())
+	return RegexMatchWithOpts(url, matchString, matchIndex)
+}
+
+func RegexMatchWithOpts(url string, matchString string, matchIndex int, opts ...optionFunction) (rawRegex string,
+	err error) {
+
+	options := Options{}
+	for _, opt := range opts {
+		opt(&options)
+	}
+
+	chromeDPOptions := chromedp.DefaultExecAllocatorOptions[:]
+
+	if options.UserAgent != "" {
+		chromeDPOptions = append(chromeDPOptions, chromedp.UserAgent(options.UserAgent))
+	}
+
+	// specify a new context set up for NewContext
+	ctx, cancel := chromedp.NewExecAllocator(context.Background(), chromeDPOptions...)
+	defer cancel()
+
+	ctx, cancel = chromedp.NewContext(context.Background())
 	defer cancel()
 
 	var res string
